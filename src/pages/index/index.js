89ef5d353b4f83a5904c5fb250e59e2327535111
ttentau <o8949488816>
2019-03-15 21:54:22 +0800

    'bf'

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,52 +1,77 @@
-import Taro, { Component } from '@tarojs/taro'
-import { View, Button, Text } from '@tarojs/components'
-import { connect } from '@tarojs/redux'
+import Taro, {Component} from '@tarojs/taro'
+import {View, Button, Text} from '@tarojs/components'
+import {connect} from '@tarojs/redux'
 
-import { add, minus, asyncAdd } from '../../actions/counter'
+import {add, minus, asyncAdd} from '../../actions/counter'
 
 import './index.scss'
 
 
-@connect(({ counter }) => ({
-  counter
+@connect(({counter}) => ({
+    counter
 }), (dispatch) => ({
-  add () {
-    dispatch(add())
-  },
-  dec () {
-    dispatch(minus())
-  },
-  asyncAdd () {
-    dispatch(asyncAdd())
-  }
+    add() {
+        dispatch(add())
+    },
+    dec() {
+        dispatch(minus())
+    },
+    asyncAdd() {
+        dispatch(asyncAdd())
+    }
 }))
 class Index extends Component {
 
     config = {
-    navigationBarTitleText: '首页'
-  }
+        navigationBarTitleText: '首页'
+    }
 
-  componentWillReceiveProps (nextProps) {
-    console.log(this.props, nextProps)
-  }
+    constructor(props) {
+        super(props)
+        this.data = [
+            1, 2, 3
+        ]
+    }
 
-  componentWillUnmount () { }
+    componentWillMount() {
+        Taro.request({
+            url: 'https://www.v2ex.com/api/topics/latest.json',
+            data: {},
+            header: {
+                'content-type': 'application/json',
+                Authentication:'none'
+            }
+        }).then(res => console.log(res.data))
+    }
 
-  componentDidShow () { }
+    componentWillReceiveProps(nextProps) {
+        console.log(this.props, nextProps)
+    }
 
-  componentDidHide () { }
+    componentWillUnmount() {
+    }
 
-  render () {
-    return (
-      <View className='index'>
-        <Button className='add_btn' onClick={this.props.add}>+</Button>
-        <Button className='dec_btn' onClick={this.props.dec}>-</Button>
-        <Button className='dec_btn' onClick={this.props.asyncAdd}>async</Button>
-        <View><Text>{this.props.counter.num}</Text></View>
-        <View><Text>Hello, World</Text></View>
-      </View>
-    )
-  }
+    componentDidShow() {
+    }
+
+    componentDidHide() {
+    }
+
+    render() {
+        const listItems = this.data.map((number) =>
+            <View><Text>{number}</Text></View>
+        )
+        return (
+            <View className='index'>
+                <Button className='add_btn' onClick={this.props.add}>+</Button>
+                <Button className='dec_btn' onClick={this.props.dec}>-</Button>
+                <Button className='dec_btn' onClick={this.props.asyncAdd}>async</Button>
+                <View><Text>{this.props.counter.num}</Text></View>
+                <View><Text>Hello, World</Text></View>
+                <View>{listItems}</View>
+            </View>
+        )
+    }
 }
 
 export default Index
